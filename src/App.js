7f533c86d5cdf49886ca2d6edfc5128241f12eb4
kp <abhiramjuly16@gmail.com>
2022-04-03 13:32:34 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 import './App.css';
 
 function App() {
-  return (
-    <div class="form-label" style={{ textAlign: "center" }}>
-        <p></p>
-        <form method="POST" action="http://localhost:5000/result" enctype="multipart/form-data"> 
-            <input type="file" name="file" class="form-control form-control-lg"/>
-            <p></p>
-            <button class="btn btn-secondary">IMPORT CSV2</button>
-        </form>
-    </div>
+    const changed=()=>{
+        console.log("value");
+    }
+    return (
+        <div class="fdd" style={{ textAlign: "center" }}>
+            <div class="rrect">
+                {/* <p class="upcsv">Upload csv</p> */}
+                <form method="POST" action="http://localhost:5000/result" enctype="multipart/form-data" class="fclass"> 
+                    <input type="file" name="file" class="btn btn-secondary" title="Choose a video please" accept=".csv"/>
+                    {/*<input type="file" id="files" />
+                    <label for="files" class="btn btn-secondary">Choose CSV file </label> */}
+                    <button class="button">Import CSV</button>
+                </form>
+                {/* <img id="output" src="" width="100" height="100"/> */}
+            </div>
+        </div>
     )
 }
 
