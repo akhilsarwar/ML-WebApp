6d15728f7398827c9c0d1d524810e5df3668c49e
kp <abhiramjuly16@gmail.com>
2022-04-03 13:51:21 +0530

    updated css2

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ function App() {
         console.log("value");
     }
     return (
-        <div class="fdd" style={{ textAlign: "center" }}>
-            <div class="rrect">
-                {/* <p class="upcsv">Upload csv</p> */}
-                <form method="POST" action="http://localhost:5000/result" enctype="multipart/form-data" class="fclass"> 
-                    <input type="file" name="file" class="btn btn-secondary" title="Choose a video please" accept=".csv"/>
-                    {/*<input type="file" id="files" />
-                    <label for="files" class="btn btn-secondary">Choose CSV file </label> */}
-                    <button class="button">Import CSV</button>
-                </form>
-                {/* <img id="output" src="" width="100" height="100"/> */}
-            </div>
+        <div class="rrect">
+            {/* <p class="upcsv">Upload csv</p> */}
+            <form method="POST" action="http://localhost:5000/result" enctype="multipart/form-data" > 
+                <input type="file" name="file" class="btn btn-secondary" title="Choose a video please" accept=".csv"/>
+                {/*<input type="file" id="files" />
+                <label for="files" class="btn btn-secondary">Choose CSV file </label> */}
+                <button class="button">Import CSV</button>
+            </form>
+            {/* <img id="output" src="" width="100" height="100"/> */}
         </div>
     )
 }
